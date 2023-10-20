87b47c2ea8aecd13c033764a9b0d75b8cc472cd9
jpcura <146609257+jpcura@users.noreply.github.com>
2023-10-19 18:16:33 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,5 +1,36 @@
-import java.io.IOExceptoin;
-import hava.new.URI;
+import java.io.IOException;
+import java.net.URI;
 
 class Handler implements URLHandler {
+    String s = "";
+    Integer n = 0;
+    public String handleRequest(URI url){
+        
+       if (url.getPath().equals("/add-messages")){
+        String[] para = url.getQuery().split("=");
+        if (para[0].equals("s")){
+            n ++;
+            s = s + "\n" + n.toString() + ". "+ para[1];
+            return s;
+        }
+
+       }
+
+       return "Waiting... /add-messages to create your list!";
+    }
+
+    
 }
+
+class StringServer{
+        public static void main(String[] args) throws IOException{
+            if(args.length == 0){
+                System.out.println("Missing port number! Try any number between 1024 to 49151");
+                return;
+            }
+
+            int port = Integer.parseInt(args[0]);
+
+            Server.start(port, new Handler());
+        }
+    }
